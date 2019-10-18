e8c3d5abf59d5b0b48ecc138d8b498c289c72595
lyd0 <1170507875@qq.com>
2019-10-18 14:50:50 +0800

    改好js

--- src/main/java/com/edmar/mll/controller/PageController.java
+++ src/main/java/com/edmar/mll/controller/PageController.java
@@ -3,6 +3,7 @@ package com.edmar.mll.controller;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,4 +21,6 @@ public class PageController implements WebMvcConfigurer {
 
         registry.addViewController("/u").setViewName("uu");
     }
+
+
 }
--- src/main/resources/static/asserts/js/index.js
+++ src/main/resources/static/asserts/js/index.js
@@ -1,25 +1,25 @@
-console.log("====index",$)
-$('#list').cascadingDropdown({
-    selectBoxes: [
-        {
-            selector: '.step1',
-            selected: '4.3'
-        },
-        {
-            selector: '.step2',
-            requires: ['.step1']
-        },
-        {
-            selector: '.step3',
-            requires: ['.step1', '.step2'],
-            onChange: function(event, value, requiredValues) {
-                // do stuff
-
-                // event is the change event object for the current dropdown
-                // value is the current dropdown value
-                // requiredValues is an object with required dropdown values
-                // requirementsMet is a boolean value to indicate if all requirements (including current dropdown having a value) have been met
-            }
-        }
-    ]
-});
+console.log("====index")
+// $('#list').cascadingDropdown({
+//     selectBoxes: [
+//         {
+//             selector: '.step1',
+//             selected: '4.3'
+//         },
+//         {
+//             selector: '.step2',
+//             requires: ['.step1']
+//         },
+//         {
+//             selector: '.step3',
+//             requires: ['.step1', '.step2'],
+//             onChange: function(event, value, requiredValues) {
+//                 // do stuff
+//
+//                 // event is the change event object for the current dropdown
+//                 // value is the current dropdown value
+//                 // requiredValues is an object with required dropdown values
+//                 // requirementsMet is a boolean value to indicate if all requirements (including current dropdown having a value) have been met
+//             }
+//         }
+//     ]
+// });
